cfc548fb354803bc0a95e794877251f05e3b3dd2
aldrinburgos <burgos.aldrin@gmail.com>
2022-05-22 18:13:23 -0500

    create connection to the database

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+//import Sequelize
+const Sequelize = require("sequelize");
+
+//use dotenv for db login credentials
+require("dotenv").config();
+console.log(process.env);
+
+//create connection to database
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PW,
+  {
+    host: "localhost",
+    dialect: "mysql",
+    port: 3306,
+  }
+);
+
+module.exports = sequelize;
---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS the_tech_blog_v2;
+
+CREATE DATABASE the_tech_blog_v2;
