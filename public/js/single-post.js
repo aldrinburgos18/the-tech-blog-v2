eab5ada3154b2d5c9efcf04b4e3c1676e15822bc
aldrinburgos <burgos.aldrin@gmail.com>
2022-06-29 00:15:01 -0500

    add comment functionality

--- public/js/single-post.js
+++ public/js/single-post.js
@@ -1,1 +1,31 @@
-console.log("loaded");
+const commentForm = document.getElementById("comment-form");
+
+async function submitOnEnter(event) {
+  if (event.which === 13 && !event.shiftKey) {
+    event.target.form.dispatchEvent(new Event("submit", { cancelable: true }));
+
+    const comment_text = commentForm.value;
+    const post_id = window.location.toString().split("/")[
+      window.location.toString().split("/").length - 1
+    ];
+
+    if (comment_text) {
+      const response = await fetch("/api/comments", {
+        method: "POST",
+        body: JSON.stringify({
+          post_id,
+          comment_text,
+        }),
+        headers: { "Content-Type": "application/json" },
+      });
+
+      if (response.ok) {
+        document.location.reload();
+      } else {
+        alert(response.statusText);
+      }
+    }
+  }
+}
+
+commentForm.addEventListener("keypress", submitOnEnter);
